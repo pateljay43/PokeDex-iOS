8288d17a482c32a5af1f70935eb2a0dba683386a
pateljay143 <pateljay143@gmail.com>
2017-05-24 20:43:56 -0700

    URL constants to access Pokedex API

---
+++ PokeDex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  PokeDex
+//
+//  Created by JAY PATEL on 5/24/17.
+//  Copyright © 2017 Jay. All rights reserved.
+//
+
+import Foundation
+
+let URL_Base = "http://pokeapi.co"
+let URL_Pokemon = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
